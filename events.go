6da09408a713661880e09ff7424d9de7c44bf3eb
muktiarafi <muktiarafi@gmail.com>
2021-05-09 22:44:46 +0700

    use protobuf as serialization format for event types

--- events.go
+++ events.go
@@ -21,41 +21,6 @@ const (
 	PaymentCreated = "payment.created"
 )
 
-type (
-	TicketCreatedEvent struct {
-		ID      int     `msgpack:"id"`
-		Version int     `msgpack:"version"`
-		Title   string  `msgpack:"title"`
-		Price   float64 `msgpack:"price"`
-		UserID  int     `msgpack:"userId"`
-	}
-
-	TicketUpdatedEvent struct {
-		ID      int     `msgpack:"id"`
-		Version int     `msgpack:"version"`
-		Title   string  `msgpack:"title"`
-		Price   float64 `msgpack:"price"`
-		UserID  int     `msgpack:"userId"`
-		OrderID int     `msgpack:"orderId"`
-	}
-
-	OrderCreatedEvent struct {
-		ID          int     `msgpack:"id"`
-		Status      string  `msgpack:"status"`
-		Version     int     `msgpack:"version"`
-		UserID      int     `msgpack:"userId"`
-		ExpiresAt   string  `msgpack:"expiresAt"`
-		TicketID    int     `msgpack:"ticketId"`
-		TicketPrice float64 `msgpack:"ticketPrice"`
-	}
-
-	OrderCancelledEvent struct {
-		ID       int `msgpack:"id"`
-		Version  int `msgpack:"version"`
-		TicketID int `msgpack:"ticketId"`
-	}
-)
-
 type EventHandler func(msg *message.Message) error
 
 func CreatePublisher(brokers []string, logger watermill.LoggerAdapter) (message.Publisher, error) {
--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 	github.com/go-playground/locales v0.13.0
 	github.com/go-playground/universal-translator v0.17.0
 	github.com/go-playground/validator/v10 v10.6.0
+	github.com/gogo/protobuf v1.3.2
 	github.com/labstack/echo/v4 v4.2.2
 )
